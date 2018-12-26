5f88dfd4bb9491414d2aaffc5396b3f56365f943
Rahman <rahmanromli@gmail.com>
2018-12-26 19:34:12 +0800

    Handling message

--- 3_window_click.c
+++ 3_window_click.c
@@ -6,6 +6,13 @@ const char g_szClassName[] = "myWindowClass";
 // Step 4: the Window Procedure
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	switch(msg) {
+		case WM_LBUTTONDOWN: {
+			char szFileName[MAX_PATH];
+			HINSTANCE hInstance = GetModuleHandle(NULL);
+
+			GetModuleFileName(hInstance, szFileName, MAX_PATH);
+			MessageBox(hwnd, szFileName, "This program is: ", MB_OK | MB_ICONINFORMATION);
+		} break;
 		case WM_CLOSE: DestroyWindow(hwnd); break;
 		case WM_DESTROY: PostQuitMessage(0); break;
 		default: return DefWindowProc(hwnd, msg, wParam, lParam);
