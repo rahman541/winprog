8e2a1c32f709bd347bce09294fa858be803d3bbd
Rahman <rahmanromli@gmail.com>
2018-12-27 19:51:40 +0800

    Prepare for Modeless Dialog window

---
+++ 7_dlg_two.c
@@ -0,0 +1,51 @@
+#include <windows.h>
+#include "7_dlg_two.h"
+
+// http://www.winprog.org/tutorial/modeless_dialogs.html
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+		case WM_DESTROY: PostQuitMessage(0); return 0;
+		default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return 0;
+}
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    PSTR lpCmdLine, INT nCmdShow) {
+	WNDCLASS wc = { };
+	wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = "myClass";
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        "myClass",                     // Window class
+        "Learn to Program Windows",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+        );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
