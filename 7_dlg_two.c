55c2eec98a1652f6fbf4ad5bb84b869dda8152c2
Rahman <rahmanromli@gmail.com>
2018-12-27 21:43:38 +0800

    Modeless dialog

--- 7_dlg_two.c
+++ 7_dlg_two.c
@@ -2,10 +2,49 @@
 #include "7_dlg_two.h"
 
 // http://www.winprog.org/tutorial/modeless_dialogs.html
+HWND g_hToolbar = NULL;
+BOOL CALLBACK ToolDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
+    switch(Message) {
+        case WM_COMMAND:
+            switch(LOWORD(wParam)) {
+                case IDC_PRESS:
+                    MessageBox(hwnd, "Hi!", "This is a message", 
+                        MB_OK | MB_ICONEXCLAMATION);
+                break;
+                case IDC_OTHER:
+                    MessageBox(hwnd, "Bye!", "This is also a message", 
+                        MB_OK | MB_ICONEXCLAMATION);
+                break;
+            }
+        break;
+        default:
+            return FALSE;
+    }
+    return TRUE;
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch (uMsg) {
-		case WM_DESTROY: PostQuitMessage(0); return 0;
+		case WM_CREATE:
+	        g_hToolbar = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_TOOLBAR), hwnd, ToolDlgProc);
+	        if(g_hToolbar != NULL)
+	            ShowWindow(g_hToolbar, SW_SHOW);
+	        else
+	            MessageBox(hwnd, "CreateDialog returned NULL", "Warning!", MB_OK | MB_ICONINFORMATION);
+	    break;
+	    case WM_COMMAND:
+	        switch(LOWORD(wParam)) {   
+	            case IDM_DIALOG_SHOW:
+	                ShowWindow(g_hToolbar, SW_SHOW);
+	            break;
+	            case IDM_DIALOG_HIDE:
+	                ShowWindow(g_hToolbar, SW_HIDE);
+	            break;
+	            //... other command handlers
+	        }
+	    break;
+		case WM_DESTROY:
+			DestroyWindow(g_hToolbar); PostQuitMessage(0); return 0;
 		default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return 0;
